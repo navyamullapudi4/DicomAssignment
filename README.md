6af23715f87266bdd9814142f9919f380a46bc26
navya mullapudi <navyamullapudi4@gmail.com>
2021-02-08 00:03:16 +0000

    readmodify

---
+++ README.md
@@ -0,0 +1,22 @@
+Description
+
+1.list_tag method will find the required tagname if the tagname is given else it will return all the tagnames in the file
+
+2.modify_tag method is used to modify the tagvalue 
+
+3.delete_tag is used for deleting the tagname in the file
+
+
+prerequisites
+
+1.python3.8
+
+2.pip 
+
+Instaling
+
+1.pip install -r requirements.txt
+
+Running
+
+1.python3 -m unittest main.py
---
+++ main.py
@@ -0,0 +1,58 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pydicom
+
+def list_tag(ds, tagname=""):
+    return ds.dir(tagname)
+
+def modify_tag(ds, tagname, tagvalue, outputfilename):
+    de = ds[tagname]
+    de.value = tagvalue
+    ds.save_as(outputfilename)
+
+def delete_tag(ds, tagname, outputfilename):
+    del ds[tagname]
+    ds.save_as(outputfilename)
+
+
+# Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+#     filename="data/693_J2KR.dcm"
+#     outputfilename = "data/693_J2KR_modified.dcm"
+#
+#     ds = pydicom.dcmread(filename)
+#     print(list_tags(ds))
+#
+#     modify_tag(ds, "PatientName","john",outputfilename)
+#     delete_tag(ds, "Columns", outputfilename)
+
+
+import unittest
+
+class TestMethods(unittest.TestCase):
+
+    def setUp(self):
+        self.filename = "data/693_J2KR.dcm"
+        self.ds = pydicom.dcmread(self.filename)
+        self.tagname = "PatientName"
+        self.outputfilename = "data/693_J2KR_modified.dcm"
+
+    def test_list_tags(self):
+        actual = ["PatientName"]
+        expected = list_tags(self.ds, self.tagname)
+        self.assertEqual(actual, expected)
+
+    def test_modify_tag(self):
+        tagvalue = "sodhigaada"
+        modify_tag(self.ds, self.tagname, tagvalue, self.outputfilename)
+        output_data = pydicom.dcmread(self.outputfilename)
+        expected = output_data[self.tagname].value
+        actual = tagvalue
+        self.assertEqual(actual, expected)
+
+    def test_delete_tag(self):
+        delete_tag(self.ds, self.tagname, self.outputfilename)
+        output_data = pydicom.dcmread(self.outputfilename)
+        self.assertFalse(self.tagname in output_data)
